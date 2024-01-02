dbdb6b8fcb0e39fdf62ddea34fdf1fb27c3c5b01
goldenpiee <matrica646@gmail.com>
2024-01-02 17:08:23 +0300

    Update quadratic_solver.py

--- src/quadratic_solver.py
+++ src/quadratic_solver.py
@@ -1,8 +1,16 @@
-import cmath
+import cmath  # Импортируем cmath для работы с комплексными числами
+
 
 def solve_quadratic_equation(a, b, c):
-    # Решение квадратного уравнения ax^2 + bx + c = 0
-    discriminant = cmath.sqrt(b**2 - 4*a*c)
-    root1 = (-b + discriminant) / (2*a)
-    root2 = (-b - discriminant) / (2*a)
-    return root1, root2
+    # Вычисляем дискриминант
+    discriminant = b ** 2 - 4 * a * c
+
+    # Проверяем значение дискриминанта
+    if discriminant >= 0:
+        # Вычисляем корни для действительных чисел
+        root1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
+        root2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+        return root1, root2
+    else:
+        # Для отрицательного дискриминанта возвращаем None
+        return None
