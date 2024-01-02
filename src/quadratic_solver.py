c912845be1d5ba93fe9288556257ea67197c1c47
goldenpiee <matrica646@gmail.com>
2024-01-02 19:11:03 +0300

    123

--- src/quadratic_solver.py
+++ src/quadratic_solver.py
@@ -1,5 +1,3 @@
-import cmath
-
 def solve_quadratic_equation(a, b, c):
     # Вычисляем дискриминант
     discriminant = b ** 2 - 4 * a * c
@@ -7,8 +5,8 @@ def solve_quadratic_equation(a, b, c):
     # Проверяем значение дискриминанта
     if discriminant >= 0:
         # Вычисляем корни для действительных чисел
-        root1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
-        root2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+        root1 = (-b + discriminant**0.5) / (2 * a)
+        root2 = (-b - discriminant**0.5) / (2 * a)
         return root1, root2
     else:
         # Для отрицательного дискриминанта возвращаем None
--- tests/test_quadratic_solver.py
+++ tests/test_quadratic_solver.py
@@ -1,17 +1,22 @@
 import unittest
 from src.quadratic_solver import solve_quadratic_equation
 
-
 class TestQuadraticSolver(unittest.TestCase):
 
     def test_real_roots_zero_discriminant(self):
-        self.assertEqual(solve_quadratic_equation(1, -2, 1), (1, 1))
+        result = solve_quadratic_equation(1, -2, 1)
+        self.assertEqual(result, (1, 1))
+        print(f"Test case 'test_real_roots_zero_discriminant' result: {result}")
 
     def test_real_roots_positive_discriminant(self):
-        self.assertEqual(solve_quadratic_equation(1, -3, 2), (2, 1))
+        result = solve_quadratic_equation(1, -3, 2)
+        self.assertEqual(result, (2, 1))
+        print(f"Test case 'test_real_roots_positive_discriminant' result: {result}")
 
     def test_real_roots_negative_discriminant(self):
-        self.assertEqual(solve_quadratic_equation(1, 1, 1), None)
+        result = solve_quadratic_equation(1, 1, 1)
+        self.assertEqual(result, None)
+        print(f"Test case 'test_real_roots_negative_discriminant' result: {result}")
 
 if __name__ == '__main__':
     unittest.main()
