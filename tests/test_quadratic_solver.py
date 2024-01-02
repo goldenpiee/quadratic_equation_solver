1f263aeec228be740eb775c225608d5ca5ac4825
goldenpiee <matrica646@gmail.com>
2024-01-02 17:02:08 +0300

    tests

--- tests/test_quadratic_solver.py
+++ tests/test_quadratic_solver.py
@@ -3,14 +3,14 @@ from src.quadratic_solver import solve_quadratic_equation
 
 class TestQuadraticSolver(unittest.TestCase):
 
-    def test_real_roots(self):
+    def test_real_roots_zero_discriminant(self):
+        self.assertEqual(solve_quadratic_equation(1, -2, 1), (1, 1))
+
+    def test_real_roots_positive_discriminant(self):
         self.assertEqual(solve_quadratic_equation(1, -3, 2), (2, 1))
 
-    def test_complex_roots(self):
-        self.assertEqual(solve_quadratic_equation(1, 1, 1), ((-0.5 + 1j * 0.866), (-0.5 - 1j * 0.866)))
-
-    def test_zero_discriminant(self):
-        self.assertEqual(solve_quadratic_equation(1, -2, 1), (1, 1))
+    def test_real_roots_negative_discriminant(self):
+        self.assertEqual(solve_quadratic_equation(1, 1, 1), None)
 
 if __name__ == '__main__':
     unittest.main()
