0ff0e704d01f5af92d8563e16e9cdce2d277d0f5
goldenpiee <155443912+goldenpiee@users.noreply.github.com>
2024-01-10 12:38:57 +0300

    Update test_quadratic_solver.py

--- tests/test_quadratic_solver.py
+++ tests/test_quadratic_solver.py
@@ -1,22 +1,26 @@
 import unittest
 from src.quadratic_solver import solve_quadratic_equation
 
+
 class TestQuadraticSolver(unittest.TestCase):
 
     def test_real_roots_zero_discriminant(self):
-        result = solve_quadratic_equation(1, -2, 1)
-        self.assertEqual(result, (1, 1))
-        print(f"Test case 'test_real_roots_zero_discriminant' result: {result}")
+        self.assertEqual(solve_quadratic_equation(1, -2, 1), (1, 1))
 
     def test_real_roots_positive_discriminant(self):
-        result = solve_quadratic_equation(1, -3, 2)
-        self.assertEqual(result, (2, 1))
-        print(f"Test case 'test_real_roots_positive_discriminant' result: {result}")
+        self.assertEqual(solve_quadratic_equation(1, -3, 2), (2, 1))
 
     def test_real_roots_negative_discriminant(self):
-        result = solve_quadratic_equation(1, 1, 1)
-        self.assertEqual(result, None)
-        print(f"Test case 'test_real_roots_negative_discriminant' result: {result}")
+        self.assertEqual(solve_quadratic_equation(1, 1, 1), None)
+
+    def test_invalid_coefficient_a(self):
+        with self.assertRaises(ValueError):
+            solve_quadratic_equation(0, 1, 1)
+
+    def test_invalid_coefficients_a_and_b(self):
+        with self.assertRaises(ValueError):
+            solve_quadratic_equation(0, 0, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
